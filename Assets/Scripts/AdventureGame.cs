9502b4f07829db9b18663466547a7aa274cd3221
Gabriel Araujo <gabrieloliveira42@hotmail.com>
2020-05-05 17:23:28 -0300

    first commit of project

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* Note:
+ * To rename everything with the same name at once use Ctrl + R + R
+ */
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text storyText;
+    [SerializeField] State startingState;
+
+    // Current State
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        storyText.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageStates();
+        QuitGame();
+    }
+
+    private void ManageStates()
+    {
+        // Get next possible states
+        State[] nextStates = state.GetNextStates();
+
+        // Iterates through next possible states
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            // If that allows Input only for possible states
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+        // Updates text to current state
+        storyText.text = state.GetStateStory();
+    }
+
+    void QuitGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Allows for creating a new state in the Unity Editor
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,14)][SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
